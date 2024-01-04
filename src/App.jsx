c1c40e6cc2ef173d1dd4d61a110f623c421ce8a5
JulianAballay <juliancabj1210@gmail.com>
2024-01-03 23:31:22 -0300

    Agrego comentarios sobre react y agrego componente en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+// Zona de imports
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  
+  // Zona de logica que queremos que tenga el componente, todo lo que sea JS
 
   return (
-    <h1 className='display-3'>Titulo de prueba</h1>
+    // Zona de maquetado y de forma no habitual logica JS
+    // Una de las reglas de componentes que nos pone react es que cada componente sabe retornada una sola
+// porcion de codigo. Es decir en este caso h1 y el section estan en el mismo nivel, por lo tanto nos rompia el codigo
+    // <section>
+    //   <h1 className='display-3'>Titulo de prueba</h1>
+    //   {/*  */}
+    // </section>
+    // Para el caso en que necesitamos maquetar dos etiquetas en un mismo nivel, react creo un elemento que se 
+    // llama onfragment <> </>. Este componente siempre retorna un solo fragmento
+    <>
+    
+    <section className='container my-4'>
+    <h1 className='display-3 text-center'>Contador React</h1>  
+       {/*  */}
+    </section>
+    </>
   )
 }
 
---
+++ src/components/TituloSecundario.jsx
@@ -0,0 +1,13 @@
+// En versiones anteriores de react era necesario importar la libreria de react
+// en la version actual no es necesario 
+import React from 'react';
+
+const TituloSecundario = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TituloSecundario;
