8790f2674580983365902d157e6d673f4977b71f
JulianAballay <juliancabj1210@gmail.com>
2024-01-05 01:13:20 -0300

    Agregamos el componente TituloSecundario

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // Zona de imports
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import TituloSecundario from "./components/TituloSecundario";
 
 function App() {
   // Zona de logica que queremos que tenga el componente, todo lo que sea JS
@@ -7,21 +8,32 @@ function App() {
   return (
     // Zona de maquetado y de forma no habitual logica JS
     // Una de las reglas de componentes que nos pone react es que cada componente sabe retornada una sola
-// porcion de codigo. Es decir en este caso h1 y el section estan en el mismo nivel, por lo tanto nos rompia el codigo
+    // porcion de codigo. Es decir en este caso h1 y el section estan en el mismo nivel, por lo tanto nos rompia el codigo
+    // <h1 className='display-3'>Titulo de prueba</h1>
     // <section>
-    //   <h1 className='display-3'>Titulo de prueba</h1>
     //   {/*  */}
     // </section>
-    // Para el caso en que necesitamos maquetar dos etiquetas en un mismo nivel, react creo un elemento que se 
+    // Para el caso en que necesitamos maquetar dos etiquetas en un mismo nivel, react creo un elemento que se
     // llama onfragment <> </>. Este componente siempre retorna un solo fragmento
     <>
-    
-    <section className='container my-4'>
-    <h1 className='display-3 text-center'>Contador React</h1>  
-       {/*  */}
-    </section>
+      <section className="container my-4">
+        <h1 className="display-3 text-center">Contador React</h1>
+        {/* Para poder traer componentes desde otros archivos, estos se traen como etiquetas.
+        Es decir que para poder utilizar estos componentes debemos importarlos para poder usarlos. Ejemplo
+        import TituloSecundario from './components/TituloSecundario.jsx' */}
+
+        {/* Los componentes se los puede escribir como etiquetas simples o dobles como este ejemplo. */}
+        {/* <TituloSecundario></TituloSecundario> */}
+        <TituloSecundario/>
+        {/* Si vamos al inspector del navegador, en la seccion components vemos cual es el componente que invoca a cual
+        debido a que hay un pequeño espacio en el componente hijo */}
+        {/* Las etiquetas HTML siempre se escriben en minuscula en comparacion de las etiqueta de componentes */}
+        {/* React tambien nos dice que los componentes se pueden reutilizar cuantas veces quieramos. Es decir
+        que si lo llamamos mas veces, este se va ejecutar las veces que sea llamado */}
+        
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/TituloSecundario.jsx
+++ src/components/TituloSecundario.jsx
@@ -1,13 +1,29 @@
 // En versiones anteriores de react era necesario importar la libreria de react
-// en la version actual no es necesario 
-import React from 'react';
+// en la version actual no es necesario
+// import React from 'react';
+// Siempre nuestros componentes se deben poder exportar
 
 const TituloSecundario = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <article>
+      <h2>Practica con react</h2>
+      <p>
+        React es una biblioteca de JavaScript desarrollada por Facebook que se
+        utiliza para construir interfaces de usuario interactivas y eficientes.
+        Su enfoque se centra en la construcción de componentes reutilizables que
+        gestionan su propio estado y pueden actualizarse de manera eficiente
+        mediante la manipulación del DOM virtual. React utiliza un modelo de
+        programación declarativa, lo que significa que los desarrolladores
+        describen cómo debería ser la interfaz de usuario y React se encarga de
+        gestionar los cambios de manera eficiente a través de su Virtual DOM.
+        Además, React es parte de un ecosistema sólido que incluye herramientas
+        como React Router para el enrutamiento y Redux para la gestión del
+        estado global. Su popularidad se debe a su capacidad para crear
+        aplicaciones web modernas y escalables, así como a una activa comunidad
+        de desarrolladores y una documentación exhaustiva.
+      </p>
+    </article>
+  );
 };
 
 export default TituloSecundario;
