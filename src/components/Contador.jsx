ad417aa00cb2eb25f6981c8ac3fad0d6b7bf3d6b
JulianAballay <juliancabj1210@gmail.com>
2024-01-05 20:50:42 -0300

    Agrego funcionalidad de incrementar el contador al hacer click en el boton en el componente contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,11 +1,21 @@
 
 
 const Contador = () => {
+    let numero = 10;
+    const incrementarValor = () =>{
+        numero++;
+        console.log(numero);
+    }
+
     return (
+        // Para insertar un variable de js en html se hace con llaves
+        // Para agregar un evento en react es desde HTML
+        // Si la funcion relacionada al evento es larga, escribirla en este seccion. Caso contrario
+        // Si es de una sola linea, escribirla en 
         <article className="text-center">
             <h2>Contador</h2>
-            <h3>10</h3>
-            <button className="btn btn-primary">+1</button>
+            <h3>{numero}</h3>
+            <button className="btn btn-primary" onClick={incrementarValor}>+1</button>
         </article>
     );
 };
